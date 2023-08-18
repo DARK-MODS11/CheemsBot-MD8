10884ca51d751111c6b8df9c7d691994bbf49a72
Abhinandh <104196277+DARK-MODS11@users.noreply.github.com>
2023-08-18 18:16:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['918943735761'] //ur owner number
+global.ownernomer = "918606102841" //ur owner number2
+global.ownername = "EX MODZ OFC" //ur owner name
+global.ytname = "YT: EX MODZ OFC" //ur yt chanel name
+global.socialm = "GitHub: DARK-MODS11" //ur github or insta name
+global.location = "India, kerala, kannur" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.ownernumber = '918943735761'
+global.ownername = 'ঔৣ͜͡⃟፝͢͡😳ঔৣ͜͡𝘼𝘽𝙃𝙄 𝙎𝙀𝙍🍁⃤'
+global.ownerNumber = ["918943735764@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@exmodzofc?si=uCtlj5lzqrAiNHGt"
+global.websitex = "https://youtu.be/jT8cuS4ohz4?si=PRIgL3tblfKLF95l"
+global.wagc = "https://chat.whatsapp.com/Iod2Tp761CWJonAshptv3o"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botscript = 'https://github.com/DARK-MODS11/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "ABHI SER\n\n"
+global.creator = "918943735761@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
